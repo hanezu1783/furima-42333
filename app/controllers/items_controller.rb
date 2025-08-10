d43d6bf12e604abeff49cc776913134969f9c93a
hanezu1783 <spiderwort1220@gmail.com>
2025-08-10 15:22:15 +0900

    findメソッドの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+
   def new
     @item = Item.new
   end
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -47,5 +46,9 @@ class ItemsController < ApplicationController
     return if user_signed_in? && current_user.id == @item.user_id
 
     redirect_to '/'
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
