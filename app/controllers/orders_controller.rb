764ef9ca7c9a665c841d1a7e215fad4c29892c8f
hanezu1783 <spiderwort1220@gmail.com>
2025-08-17 13:57:14 +0900

    テストコード

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,8 +26,8 @@ class OrdersController < ApplicationController
   end
 
   def order_address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number)
-          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token)
+          .merge(user_id: current_user.id, item_id: @item.id)
   end
 
   def pay_item
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,10 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number,
-                :user_id, :item_id, :token
 
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building,
+                :phone_number, :user_id, :item_id, :token
+
+  # 必須チェック
   with_options presence: true do
     validates :token
     validates :postal_code
@@ -14,10 +16,14 @@ class OrderAddress
     validates :item_id
   end
 
-  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+  # 郵便番号：空チェック後に形式チェック
+  validates :postal_code, format: {
+    with: /\A\d{3}-\d{4}\z/,
+    message: 'is invalid. Enter it as follows (e.g. 123-4567)'
+  }
 
+  # 電話番号：空チェック後に桁数と形式チェック
   validates_length_of :phone_number, minimum: 10, message: 'is too short'
-
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
 
   def save
